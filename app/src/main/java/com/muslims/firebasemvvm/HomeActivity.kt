3312d91e6029cd480541433076df3ae8df9fa369
LibGdxEngine <libgdxengine@gmail.com>
2022-02-28 04:07:21 +0200

    fix some navigation problems in HomeActivity and add materialDialog for exit applications when back button is pressed

--- app/src/main/java/com/muslims/firebasemvvm/HomeActivity.kt
+++ app/src/main/java/com/muslims/firebasemvvm/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.muslims.firebasemvvm
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -11,6 +12,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesome
 import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesomeBrand
 import com.mikepenz.materialdrawer.Drawer
@@ -29,6 +31,8 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
     private lateinit var navView: BottomNavigationView
     var filterDrawer: View? = null
     var mainDrawer: Drawer? = null
+    var currentFragment = R.id.navigation_home
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DrawerLayoutBinding.inflate(layoutInflater)
@@ -36,7 +40,7 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
         binding.includedHome.toolbar.setTitle(R.string.app_name)
         setSupportActionBar(binding.includedHome.toolbar)
-        
+
 
         navView = binding.includedHome.navView
 
@@ -45,7 +49,10 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_advices, R.id.navigation_profile, R.id.savedApplicationsFragment
+                R.id.navigation_home,
+                R.id.navigation_advices,
+                R.id.navigation_profile,
+                R.id.savedApplicationsFragment
             )
         )
 
@@ -55,43 +62,55 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
+            currentFragment = destination.id
             val fullScreenFragments = arrayOf(
-                    R.id.questionsFragment,
-                    R.id.welcomeToAppStepperFragment,
-                    R.id.loginFragment,
-                    )
-            if(destination.id in fullScreenFragments){
+                R.id.questionsFragment,
+                R.id.welcomeToAppStepperFragment,
+                R.id.loginFragment,
+            )
+            if (destination.id in fullScreenFragments) {
                 enterFullScreen(true);
-            }else{
+            } else {
                 enterFullScreen(false)
             }
         }
     }
 
     private fun enterFullScreen(isFullScreen: Boolean) {
-        if(isFullScreen) supportActionBar?.hide() else supportActionBar?.show()
-        if(isFullScreen) navView.visibility = View.GONE else navView.visibility = View.VISIBLE
+        if (isFullScreen) supportActionBar?.hide() else supportActionBar?.show()
+        if (isFullScreen) navView.visibility = View.GONE else navView.visibility = View.VISIBLE
     }
 
-    private fun initMainDrawer() : Drawer{
+    private fun initMainDrawer(): Drawer {
         //create the drawer and remember the `Drawer` result object
         val drawer1 = DrawerBuilder()
             .withActivity(this)
             .withToolbar(binding.includedHome.toolbar)
             .addDrawerItems(
-                PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
-                PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
-                PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withIdentifier(5),
+                PrimaryDrawerItem().withName(R.string.drawer_item_home)
+                    .withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
+                PrimaryDrawerItem().withName(R.string.drawer_item_free_play)
+                    .withIcon(FontAwesome.Icon.faw_gamepad),
+                PrimaryDrawerItem().withName(R.string.drawer_item_custom)
+                    .withIcon(FontAwesome.Icon.faw_eye).withIdentifier(5),
                 SectionDrawerItem().withName(R.string.drawer_item_section_header),
-                SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
-                SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).withEnabled(true),
+                SecondaryDrawerItem().withName(R.string.drawer_item_settings)
+                    .withIcon(FontAwesome.Icon.faw_cog),
+                SecondaryDrawerItem().withName(R.string.drawer_item_help)
+                    .withIcon(FontAwesome.Icon.faw_question).withEnabled(true),
                 SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(
-                    FontAwesomeBrand.Icon.fab_github),
-                SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
+                    FontAwesomeBrand.Icon.fab_github
+                ),
+                SecondaryDrawerItem().withName(R.string.drawer_item_contact)
+                    .withIcon(FontAwesome.Icon.faw_bullhorn)
             )
             .withStickyHeader(layoutInflater.inflate(R.layout.header, null))
             .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
-                override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*>): Boolean {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
                     Toast.makeText(applicationContext, "${position}", Toast.LENGTH_SHORT).show()
                     return false
                 }
@@ -100,7 +119,7 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
         return drawer1
     }
 
-    private fun initFilterDrawer() : View {
+    private fun initFilterDrawer(): View {
 
         binding.btnApply.setOnClickListener {
             binding.drawerLayout.closeDrawer(GravityCompat.END)
@@ -110,7 +129,7 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
             binding.drawerLayout.closeDrawer(GravityCompat.END)
         }
 
-        binding.includedHome.filterButton.setOnClickListener{
+        binding.includedHome.filterButton.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.END)
         }
         return binding.drawerLayout
@@ -121,6 +140,37 @@ class HomeActivity : AppCompatActivity(), DrawerLocker {
             if (enabled) DrawerLayout.LOCK_MODE_UNLOCKED else DrawerLayout.LOCK_MODE_LOCKED_CLOSED
         binding.drawerLayout.setDrawerLockMode(lockMode)
         mainDrawer?.drawerLayout?.setDrawerLockMode(lockMode)
+    }
+
+    override fun onBackPressed() {
+        val mainFragments = arrayOf(
+            R.id.navigation_advices,
+            R.id.savedApplicationsFragment,
+            R.id.navigation_profile,
+        )
+        if (currentFragment in mainFragments) {
+            val navController = findNavController(R.id.nav_host_fragment_activity_home)
+            navController.navigate(R.id.navigation_home)
+        } else if (currentFragment == R.id.navigation_home) {
+            showMaterialExitMessage(this)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun showMaterialExitMessage(ctx: Context) {
+
+        MaterialAlertDialogBuilder(ctx)
+            .setTitle("هل تريد الخروج فعلا ؟")
+            .setMessage("")
+            .setPositiveButton(
+                "البقاء"
+            ) { dialogInterface, i -> dialogInterface.dismiss() }
+            .setNegativeButton(
+                "خروج"
+            ) { dialogInterface, i -> finish() }
+            .show()
+
     }
 
 }
